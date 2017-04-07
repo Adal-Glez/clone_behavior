baca5fec6bd6fa2b404a91b0c96bdf2573e3831f
adalberto-g <alebx32@gmail.com>
2017-04-06 21:51:55 -0500

    add generator to habdle large number of images

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import csv
 import numpy as np
 import cv2
+import sklearn
 
 lines =[]
 with open("data/driving_log.csv") as csvfile:
@@ -35,6 +36,37 @@ for image, measurement in zip(images, measurements):
 
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_mesurements)
+
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = './IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
+ch, row, col = 3, 80, 320  # Trimmed image format
+
 
 from keras.models import Sequential, Model
 from keras.layers import Dense, Flatten, Lambda
@@ -82,8 +114,11 @@ plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
 plt.show()
 
-model.fit(X_train, y_train, validation_split=0.2, shuffle= True, nb_epoch=epochs)
+#model.fit(X_train, y_train, validation_split=0.2, shuffle= True, nb_epoch=epochs)
 
+model.fit_generator(train_generator, samples_per_epoch= /
+            len(train_samples), validation_data=validation_generator, /
+            nb_val_samples=len(validation_samples), nb_epoch=3)
 
 
 model.save('model.h5')
