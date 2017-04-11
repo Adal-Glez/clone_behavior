9786ad54620b45c192a1bb65f937442dea7bf481
adalberto-g <alebx32@gmail.com>
2017-04-11 09:55:33 -0500

    include mirrored image

--- model.py
+++ model.py
@@ -18,7 +18,7 @@ train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 print(len(lines))
 print(len(train_samples))
 
-def generator(samples, batch_size=32):
+def generator(samples, batch_size=128):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         shuffle(samples)
@@ -29,18 +29,20 @@ def generator(samples, batch_size=32):
             angles = []
             for batch_sample in batch_samples:
                 for i in range(3):
-                    name = 'data/IMG/'+batch_sample[0].split('/')[-1]
-                    center_image = cv2.imread(name)
-                    #center_angle = float(batch_sample[3])
-                    images.append(center_image)
+                    name = 'data/IMG/'+batch_sample[i].split('/')[-1]
+                    image = cv2.imread(name)
+                    images.append(image)
                     correction=0
                     if i==1:
-                        correction=0.2
+                        correction=0.4
                     if i==2:
-                        correction=-0.2
+                        correction=-0.4
                     angle = float(batch_sample[3])+correction
                     angles.append(angle)
-                    #angles.append(center_angle)
+                    
+                    images.append(np.fliplr(image)) 
+                    angles.append(angle*-1.0)
+            
 
             # trim image to only see section with road
             X_train = np.array(images)
@@ -48,10 +50,11 @@ def generator(samples, batch_size=32):
             yield sklearn.utils.shuffle(X_train, y_train)
 
 
+
+            
 # compile and train the model using the generator function
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
-
 
 
 from keras.models import Sequential, Model
@@ -61,9 +64,8 @@ from keras.layers import MaxPooling2D
 from keras.layers import Cropping2D
 import matplotlib.pyplot as plt
 
-#epochs = 3
-ch, row, col = 3, 80, 320
-#input_shape = (row, col, ch)
+epochs = 5
+
 input_shape=(160,320,3)
 print("setting model")
 #based on NVIDIA Architecture
@@ -85,11 +87,8 @@ print("compiling model")
 
 model.compile(loss='mse', optimizer = 'adam')
 print("fitting model")
-#model.fit(X_train, y_train, validation_split=0.2, shuffle= True, nb_epoch=epochs)
-model.fit_generator(train_generator, samples_per_epoch= len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
+model.fit_generator(train_generator, samples_per_epoch= len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=epochs)
 print("save model")
 
 model.save('model.h5')
 exit()
-
-
